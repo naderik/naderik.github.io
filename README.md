33c49a6dc4435fa9e3f6b94bb40ed4ed87a9ec70
Kasra Naderi <naderik@uwindsor.ca>
2021-06-28 11:17:49 -0400

    fetch data from github API

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Personal E-Porfolio React App
+
+#Firebase SDK
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.6.8/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.6.8/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+
+
+firebase login
+
+firebase init 
+
+When you’re ready, deploy your web app
+Put your static files (e.g., HTML, CSS, JS) in your app’s deploy directory (the default is “public”). Then, run this command from your app’s root directory:
+
+firebase deploy
--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useCallback } from "react";
 import './App.css';
 
+const avatarUrl = `http://github.com/naderik.png`
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="http://github.com/naderik.png" className="App-logo" alt="logo" />
-        <p>
-          Personal E-Portfolio !
-        </p>
-      </header>
-    </div>
-  );
-}
+
+  const [data, setData] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch(`https://api.github.com/users/naderik`)
+      .then((response) => response.json())
+      .then(data => setData(data))
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
+  console.log(data)
+
+  if (data) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={avatarUrl} className="App-logo" alt="logo" />
+          <p>
+            Personal E-Portfolio !
+          </p>
+          <p>My name is {data.name}</p>
+        </header>
+      </div>
+    );
+
+  } else {
+    return(
+      <h1>Loading...</h1>
+    )
+  }
+
+
+
+};
 
 export default App;
