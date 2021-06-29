4ba14f1550fffedaa4b4a10f8cb8dfd37897a43b
Kasra Naderi <naderik@uwindsor.ca>
2021-06-28 20:17:50 -0400

    add background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import HomePage from './pages/HomePage';
 import './App.css';
 import { BrowserRouter as Router , Route} from 'react-router-dom'
+import background from './img/background.png'
+
 
 
 
@@ -8,7 +10,7 @@ function App() {
 
   return(
     <Router>
-      <div className="App">
+      <div className="App" style={{backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
       
         <Route path="/" component={HomePage} exact />
       
