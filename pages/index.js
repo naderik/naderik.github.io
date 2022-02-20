d0741ea9040c2aceb35cb499e6e1af37108f776d
Kasra Naderi <kasra@yuser.co>
2022-02-20 00:31:44 -0500

    add info page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-center text-9xl font-bold underline ">
+    <h1 className="text-center text-9xl font-sans font-bold underline ">
       Hello world!
     </h1>
   )
---
+++ pages/info.js
@@ -0,0 +1,7 @@
+export default function Info() {
+  return (
+    <div className="text-center text-3xl font-sans font-bold underline ">
+      This is the info page!
+    </div>
+  )
+}
